d7295cbe211b8b48ca33911b3cec9ac0bed1ab76
MooseOnTheRocks <sam.foltz@hotmail.com>
2022-02-22 04:52:35 -0800

    Added more growable blocks for crystals

--- src/main/java/dev/foltz/crystalgrowing/crystal/CrystalTypes.java
+++ src/main/java/dev/foltz/crystalgrowing/crystal/CrystalTypes.java
@@ -12,22 +12,22 @@ public class CrystalTypes {
     public static final IntProperty GROWTH_STAGES_5 = IntProperty.of("growth_stage", 0, 4);
 
     public static final CrystalType REDSTONE_CRYSTAL_TYPE = createCrystalType(
-        List.of(Blocks.REDSTONE_ORE, Blocks.DEEPSLATE_REDSTONE_ORE), 5, 2, GROWTH_STAGES_5, CrystalGrowingMod.REDSTONE_CRYSTAL_PARTICLE,
+        List.of(Blocks.REDSTONE_ORE, Blocks.DEEPSLATE_REDSTONE_ORE, Blocks.REDSTONE_BLOCK), 5, 2, GROWTH_STAGES_5, CrystalGrowingMod.REDSTONE_CRYSTAL_PARTICLE,
         8, 14, 2, 10
     );
 
     public static final CrystalType COAL_CRYSTAL_TYPE = createCrystalType(
-        List.of(Blocks.COAL_ORE, Blocks.DEEPSLATE_COAL_ORE), 5, 1.5f, GROWTH_STAGES_5, CrystalGrowingMod.COAL_CRYSTAL_PARTICLE,
+        List.of(Blocks.COAL_ORE, Blocks.DEEPSLATE_COAL_ORE, Blocks.COAL_BLOCK), 5, 1.5f, GROWTH_STAGES_5, CrystalGrowingMod.COAL_CRYSTAL_PARTICLE,
         8, 13, 2, 12
     );
 
     public static final CrystalType IRON_CRYSTAL_TYPE = createCrystalType(
-            List.of(Blocks.IRON_ORE, Blocks.DEEPSLATE_IRON_ORE), 5, 3, GROWTH_STAGES_5, CrystalGrowingMod.IRON_CRYSTAL_PARTICLE,
+            List.of(Blocks.IRON_ORE, Blocks.DEEPSLATE_IRON_ORE, Blocks.IRON_BLOCK), 5, 3, GROWTH_STAGES_5, CrystalGrowingMod.IRON_CRYSTAL_PARTICLE,
             8, 12, 2, 9
     );
 
     public static final CrystalType GOLD_CRYSTAL_TYPE = createCrystalType(
-            List.of(Blocks.GOLD_ORE, Blocks.DEEPSLATE_GOLD_ORE, Blocks.NETHER_GOLD_ORE), 5, 2, GROWTH_STAGES_5, CrystalGrowingMod.GOLD_CRYSTAL_PARTICLE,
+            List.of(Blocks.GOLD_ORE, Blocks.DEEPSLATE_GOLD_ORE, Blocks.NETHER_GOLD_ORE, Blocks.RAW_GOLD_BLOCK, Blocks.GOLD_BLOCK), 5, 2, GROWTH_STAGES_5, CrystalGrowingMod.GOLD_CRYSTAL_PARTICLE,
             8, 12, 2, 7
     );
 
